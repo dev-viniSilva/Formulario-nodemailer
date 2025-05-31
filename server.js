b27c6aa01a3c24d4e133a13d2e31c6de380cc574
vinicius <vin123@gmail.com>
2025-05-31 19:53:45 -0400

    Configura .gitignore para ignorar node_modules e .env

---
+++ server.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const express = require('express');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+app.use(express.json());
+const cors = require('cors');
+
+app.use(cors({
+  origin: '*'
+}));
+
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.SENHA
+  }
+});
+app.get('/teste', (req, res) => {
+  res.send('Servidor ativo');
+});
+
+app.post('/enviar', async (req, res) => {
+  const { nome, sobrenome, idade, cep, rua, bairro, cidade, estado } = req.body;
+
+  const formularioPreenchedo = {
+    from: process.env.EMAIL,
+    to: process.env.EMAIL,
+    subject: 'Novo corredor para o NY75',
+    text: `
+      Nome: ${nome} ${sobrenome}
+      Idade: ${idade}
+      CEP: ${cep}
+      Rua: ${rua}
+      Bairro: ${bairro}
+      Cidade: ${cidade}
+      Estado: ${estado}
+    `
+  };
+
+  try {
+    await transporter.sendMail(formularioPreenchedo);
+    res.status(200).send('Email enviado com sucesso!');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao enviar o email');
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
